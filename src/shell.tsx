b5fedc09dcb2365590a5c4aa6d757df4b512a966
Mary <git@mary.my.id>
2024-10-31 13:37:40 +0700

    fix: have the decency to put up a spinner

--- src/shell.tsx
+++ src/shell.tsx
@@ -12,7 +12,32 @@ const Shell = () => {
 					render={({ def }) => {
 						return (
 							<ErrorBoundary fallback={(error, reset) => <ErrorPage error={error} reset={reset} />}>
-								<Suspense>
+								<Suspense
+									fallback={
+										<div class="grid grow place-items-center">
+											<svg viewBox="0 0 32 32" class="animate-spin" style="height:24px;width:24px">
+												<circle
+													cx="16"
+													cy="16"
+													fill="none"
+													r="14"
+													stroke-width="4"
+													class="stroke-purple-600 opacity-20"
+												/>
+												<circle
+													cx="16"
+													cy="16"
+													fill="none"
+													r="14"
+													stroke-width="4"
+													stroke-dasharray="80px"
+													stroke-dashoffset="60px"
+													class="stroke-purple-600"
+												/>
+											</svg>
+										</div>
+									}
+								>
 									<def.component />
 								</Suspense>
 							</ErrorBoundary>
