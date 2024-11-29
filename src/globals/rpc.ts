26d1610867c0c530edec9362b404a9b7a354ef57
Mary <git@mary.my.id>
2024-11-29 14:17:15 +0700

    chore: add local env variables

--- src/globals/rpc.ts
+++ src/globals/rpc.ts
@@ -1,5 +1,5 @@
 import { simpleFetchHandler, XRPC } from '@atcute/client';
 
 const APPVIEW_URL = import.meta.env.VITE_APPVIEW_URL;
-console.log(import.meta.env);
+
 export const appViewRpc = new XRPC({ handler: simpleFetchHandler({ service: APPVIEW_URL }) });
