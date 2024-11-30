0c59d394dbcdf8d331321f25116ce9229652c08d
Mary <git@mary.my.id>
2024-11-30 16:38:06 +0700

    refactor: redo icons

--- src/components/ic-icons/baseline-check.tsx
+++ src/components/ic-icons/baseline-check.tsx
@@ -1,5 +1,9 @@
 import { createIcon } from './_icon';
 
-const CheckIcon = createIcon([['M9 16.17L4.83 12l-1.42 1.41L9 19L21 7l-1.41-1.41z']]);
+const CheckIcon = createIcon(() => (
+	<svg width="1em" height="1em" viewBox="0 0 24 24">
+		<path fill="currentColor" d="M9 16.17L4.83 12l-1.42 1.41L9 19L21 7l-1.41-1.41z" />
+	</svg>
+));
 
 export default CheckIcon;
